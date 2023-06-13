391bf55be630953a4277530ddf4675fc2d206a19
Bhanu2203 <suryadevi0703@gmail.com>
2023-06-13 12:56:50 +0530

    bhanu

--- Cleaning/POS/Booking/Tabs/AddItem/AddListeners/AddItem/CheckBeforeSave.js
+++ Cleaning/POS/Booking/Tabs/AddItem/AddListeners/AddItem/CheckBeforeSave.js
@@ -2,14 +2,42 @@ let StartFunc = ({ inCurrentTarget }) => {
     let jVarLocalClosestTabPane = inCurrentTarget.closest(".tab-pane");
     let jVarLocalPcsClass = jVarLocalClosestTabPane.querySelector(".PcsClass");
 
+    // if (parseInt(jVarLocalPcsClass.value) > 0 === false) {
+    //     jVarLocalPcsClass.classList.add("is-invalid");
+    //     jVarLocalPcsClass.focus();
+    //     return false;
+    // };
+
+    if (jFLocalForPcs({ inClosestTabPane: jVarLocalClosestTabPane }) === false) return false;
+    if (jFLocalForRate({ inClosestTabPane: jVarLocalClosestTabPane }) === false) return false;
+
+    return true;
+};
+
+let jFLocalForPcs = ({ inClosestTabPane }) => {
+    let jVarLocalPcsClass = inClosestTabPane.querySelector(".PcsClass");
+
     if (parseInt(jVarLocalPcsClass.value) > 0 === false) {
         jVarLocalPcsClass.classList.add("is-invalid");
         jVarLocalPcsClass.focus();
         return false;
     };
 
-    console.log("aaaaaaaaaaaa", inCurrentTarget, jVarLocalClosestTabPane, jVarLocalPcsClass);
     return true;
 };
+
+let jFLocalForRate = ({ inClosestTabPane }) => {
+    let jVarLocalPcsClass = inClosestTabPane.querySelector(".RateClass");
+
+    if (parseInt(jVarLocalPcsClass.value) > 0 === false) {
+        jVarLocalPcsClass.classList.add("is-invalid");
+        jVarLocalPcsClass.focus();
+        return false;
+    };
+
+    return true;
+};
+
+
 
 export { StartFunc };
