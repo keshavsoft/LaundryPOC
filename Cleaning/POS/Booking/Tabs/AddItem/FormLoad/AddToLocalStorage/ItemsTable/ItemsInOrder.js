cec83374067e36c9048de3eedce674b8f93e5bab
keshavsoft <nknnkumar@yahoo.com>
2023-06-14 13:15:00 +0530

    keshav

--- Cleaning/POS/Booking/Tabs/AddItem/FormLoad/AddToLocalStorage/ItemsTable/ItemsInOrder.js
+++ Cleaning/POS/Booking/Tabs/AddItem/FormLoad/AddToLocalStorage/ItemsTable/ItemsInOrder.js
@@ -4,8 +4,4 @@ let StartFunc = () => {
     localStorage.setItem("ItemsInOrder", JSON.stringify(data));
 };
 
-let StartFunc_Keshav_24Apr2023 = () => {
-    localStorage.setItem("ItemsInOrder", JSON.stringify(data));
-};
-
 export { StartFunc };
