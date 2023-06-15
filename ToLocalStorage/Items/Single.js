b146fb2751991f95b3080cb2e25b5d6882bdc4d7
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-15 13:02:01 +0530

    keshav

--- ToLocalStorage/Items/Single.js
+++ ToLocalStorage/Items/Single.js
@@ -9,9 +9,24 @@ let StartFunc = ({ inEntry }) => {
     let jVarLocalNumbers = toNumbers(jVarLocalKeys);
     const max = Math.max(...jVarLocalNumbers);
 
-    jVarLocalItemData[max] = inEntry;
+    jVarLocalItemData[max + 1] = jFLocalPrepareData({ inEntry });
+
+    jFLocalPrepareData({ inEntry });
 
     StartFuncBulk({ inData: jVarLocalItemData });
+};
+
+let jFLocalPrepareData = ({ inEntry }) => {
+    let jVarLocalFromLocalStorage = localStorage.getItem(CommonKeys.ItemKey);
+    let jVarLocalItemData = JSON.parse(jVarLocalFromLocalStorage);
+    let jVarLocalvalues = Object.values(jVarLocalItemData);
+    let jVarLocalPkArray = jVarLocalvalues.map(element => element.pk);
+    const max = Math.max(...jVarLocalPkArray);
+
+    return {
+        ...inEntry,
+        pk: max + 1
+    };
 };
 
 export { StartFunc };
