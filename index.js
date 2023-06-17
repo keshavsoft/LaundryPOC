b7eb2b33a060e967599a1152f81a75b71664116d
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-17 20:16:24 +0530

    keshav

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { StartFunc as StartFuncToLocalStorage } from "./ToLocalStorage/Items.js";
+import { StartFunc as StartFuncCustomers } from "./ToLocalStorage/Customers/Bulk.js";
 
 const StartFunc = async () => {
     let jVarLocalHtmlId = "LoadDataId";
@@ -9,52 +10,34 @@ const StartFunc = async () => {
 const StartFuncLoad = async () => {
     const dirHandle = await window.showDirectoryPicker();
 
-    // for await (const entry of dirHandle.values()) {
-    //     entry.getFile().then(PromiseData => {
-    //         switch (PromiseData.name) {
-    //             case "Items.json":
-    //                 PromiseData.text().then(FileData => {
-    //                     console.log(FileData);
-    //                 });
-    //                 break;
+    await jFLocalForItems({ indirHandle: dirHandle });
+    await jFLocalForCustomers({ indirHandle: dirHandle });
 
-    //             default:
-    //                 break;
-    //         };
-    //     });
-    // };
+    // const ConfigHandle = await dirHandle.getFileHandle('Items.json');
+    // let jVarLocalgetFile = await ConfigHandle.getFile();
 
+    // let jVarLocalgetFileData = await jVarLocalgetFile.text();
 
-    const ConfigHandle = await dirHandle.getFileHandle('Items.json');
+    // StartFuncToLocalStorage({ inData: JSON.parse(jVarLocalgetFileData) });
+
+};
+
+const jFLocalForItems = async ({ indirHandle }) => {
+    const ConfigHandle = await indirHandle.getFileHandle('Items.json');
     let jVarLocalgetFile = await ConfigHandle.getFile();
 
     let jVarLocalgetFileData = await jVarLocalgetFile.text();
 
     StartFuncToLocalStorage({ inData: JSON.parse(jVarLocalgetFileData) });
-    // const AdminHandle = await ConfigHandle.getDirectoryHandle('Masters');
-    // console.log("AdminHandle  : ", AdminHandle);
-
 };
 
+const jFLocalForCustomers = async ({ indirHandle }) => {
+    const ConfigHandle = await indirHandle.getFileHandle('Customers.json');
+    let jVarLocalgetFile = await ConfigHandle.getFile();
 
-const StartFuncLoad_Keshav_15Jun = async () => {
-    const dirHandle = await window.showDirectoryPicker();
+    let jVarLocalgetFileData = await jVarLocalgetFile.text();
 
-    for await (const entry of dirHandle.values()) {
-        entry.getFile().then(PromiseData => {
-            switch (PromiseData.name) {
-                case "Items.json":
-                    PromiseData.text().then(FileData => {
-                        console.log(FileData);
-                    });
-                    break;
-
-                default:
-                    break;
-            };
-        });
-    };
+    StartFuncCustomers({ inData: JSON.parse(jVarLocalgetFileData) });
 };
-
 
 StartFunc().then();
